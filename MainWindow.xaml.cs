c6c51180a09bbf252df42d45eff57e9b0948da3c
noitaro <noitaro@example.com>
2020-11-27 12:54:37 +0900

    first commit

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Windows;
+using System.Linq;
+using System.Collections.ObjectModel;
+using System.Windows.Controls;
+using System.Collections.Generic;
+using MaterialDesignThemes.Wpf;
+using System.Threading.Tasks;
+
+namespace WpfApp3
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+    }
+}
